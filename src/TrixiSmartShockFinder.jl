bed54c6a37ba1c82dcef0e2101d840334b879014
Michael Schlottke-Lakemper <michael@sloede.com>
2023-11-03 15:44:59 +0100

    Add more exports

--- src/TrixiSmartShockFinder.jl
+++ src/TrixiSmartShockFinder.jl
@@ -3,7 +3,9 @@ module TrixiSmartShockFinder
 using MuladdMacro: @muladd
 using Trixi
 using Trixi: AbstractIndicator, AbstractEquations, AbstractSemidiscretization, @threaded,
-             summary_box, trixi_include
+             summary_box, eachdirection, get_node_vars, multiply_scalar_dimensionwise!,
+             apply_smoothing!, gauss_lobatto_nodes_weights, polynomial_interpolation_matrix,
+             trixi_include
 
 include("indicators.jl")
 include("indicators_1d.jl")
