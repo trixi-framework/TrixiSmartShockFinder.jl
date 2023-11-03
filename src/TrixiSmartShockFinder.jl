c97b57d5ae45eb4ba7f10a0a0fd8c1bf803f78a7
Michael Schlottke-Lakemper <michael@sloede.com>
2023-11-03 16:08:40 +0100

    Add Flux.jl to deps

--- src/TrixiSmartShockFinder.jl
+++ src/TrixiSmartShockFinder.jl
@@ -1,11 +1,12 @@
 module TrixiSmartShockFinder
 
 using MuladdMacro: @muladd
+using Flux: params
 using Trixi
 using Trixi: AbstractIndicator, AbstractEquations, AbstractSemidiscretization, @threaded,
              summary_box, eachdirection, get_node_vars, multiply_scalar_dimensionwise!,
              apply_smoothing!, gauss_lobatto_nodes_weights, polynomial_interpolation_matrix,
-             trixi_include
+             has_any_neighbor, has_neighbor, has_children, trixi_include
 
 include("indicators.jl")
 include("indicators_1d.jl")
